9fcfe43fd1a4b41501c9bae815db705d3f5b33e1
bdshakhawat <mdshakwat.hossain@gmail.com>
2023-03-05 23:06:55 +0600

    tailwind commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
-
+import { useState } from "react";
+import useMediaQuery from './hooks/useMediaQuery';
 function App() {
+  const [selectedPage, setSelectedPage]=useState('home');
+  const isAboveMediumScreens=useMediaQuery("(min-width:1060px)");
   return (
     <div className="App">
     </div>
